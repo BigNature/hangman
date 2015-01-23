5352686e1c951a885bda4afd4fc70c19d6ca454f
user <user@localhost.localdomain>
2015-01-23 14:58:12 +0900

    hi

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ class check(unittest.TestCase):
     def test_false(self):
         ret = hangman.checkCorrectAnswer('ta','cat')
         self.assertEqual(ret,False)
-=======
+
 
 import unittest
 import hangman
@@ -34,7 +34,7 @@ class TestCase(unittest.TestCase):
     def test_nwrongAnswer(self):
         answer = hangman.checkWrongAnswer("zebra", "zebra")
         self.assertEqual(answer, False)
->>>>>>> 8a33bd6a4d6b5cbd1855ed2c8d0bd4b72de82ab2
+
 
 
 if __name__ == "__main__":
